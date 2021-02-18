156695da292e72fe3ca077732b03f504b8219c6b
Taqin86 <63039420+Taqin86@users.noreply.github.com>
2021-02-18 20:33:30 +0700

    animasi untuk scroll

---
+++ script/navbar-scroll.js
@@ -0,0 +1,7 @@
+// function untuk animasi scroll
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
